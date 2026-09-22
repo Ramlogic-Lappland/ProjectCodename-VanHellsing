8039e62708c8975c737797c79a441978cba032bc
Lappland <B4d4ssG04t@gmail.com>
2026-09-22 07:11:54 -0300

    Addition of WinCOnditionManager
    Added a WinConditionManager that takes into account all enemies on scene and if they are defeated prints in console they have been defeated.
    Added a text in screen that tells you how many enemies are left

---
+++ Assets/Scripts/WinConditionManager.cs
@@ -0,0 +1,49 @@
+using TMPro;
+using UnityEngine;
+
+public class WinConditionManager : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI enemiesRemainingText;
+
+    private int _remainingEnemies;
+
+    private void Start()
+    {
+        _remainingEnemies =
+            FindObjectsByType<BasicEnemyBehaviour>(
+                FindObjectsSortMode.None
+            ).Length;
+
+        UpdateEnemyCounter();
+        CheckWinCondition();
+    }
+
+    public void EnemyDefeated()
+    {
+        _remainingEnemies--;
+
+        UpdateEnemyCounter();
+        CheckWinCondition();
+    }
+
+    private void UpdateEnemyCounter()
+    {
+        enemiesRemainingText.text =
+            $"Enemies Remaining: {_remainingEnemies}";
+    }
+
+    private void CheckWinCondition()
+    {
+        if (_remainingEnemies <= 0)
+        {
+            WinGame();
+        }
+    }
+
+    private void WinGame()
+    {
+        Debug.Log("YOU WIN!");
+        
+        // TODO: Show win screen, pause gameplay
+    }
+}
